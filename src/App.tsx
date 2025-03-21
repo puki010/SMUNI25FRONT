cdf5544fa3c36b8c43f9a2893459d22eeb3fc39c
bekri <bekri049@proton.me>
2025-03-21 10:27:31 +0300

    sizemod

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div> 
-      <h1 className="reward">congratulations🎉️</h1>
-      <h2 className='reward'>+0.1 TON</h2>
+      <h2 className="reward">congratulations🎉️</h2>
+      <h1 className='reward'>+0.1 TON</h1>
       <p className="read-the-docs">
       ከታች ያለውን Connect Wallet የሚለውን በመንካት ዋሌት አድሬሶን አስመዝግበውን ገንዘብዎን ማውጣት ይችላሉ።
       </p>
